b56d2eb7dd8dfced07b5a13a74415a726b665649
gspvsr <153313380+gspvsr@users.noreply.github.com>
2024-01-30 01:16:29 -0600

    practice

--- data.tf
+++ data.tf
@@ -3,6 +3,6 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
-output "azs" {
-  value = data.aws_availability_zones.available.names
+data "aws_vpc" "default" {
+  default = true 
 }
--- main.tf
+++ main.tf
@@ -67,10 +67,10 @@ resource "aws_subnet" "database"{
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
-  }
+  # route {
+  #   cidr_block = "0.0.0.0/0"
+  #   gateway_id = aws_internet_gateway.main.id
+  # }
 
   tags = merge (
         var.common_tags,
@@ -104,10 +104,10 @@ resource "aws_nat_gateway" "main" {
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.main.id
-  }
+  # route {
+  #   cidr_block = "0.0.0.0/0"
+  #   nat_gateway_id = aws_nat_gateway.main.id
+  # }
 
   tags = merge (
         var.common_tags,
@@ -121,10 +121,10 @@ resource "aws_route_table" "private" {
 resource "aws_route_table" "database" {
   vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-     nat_gateway_id = aws_nat_gateway.main.id
-  }
+  # route {
+  #   cidr_block = "0.0.0.0/0"
+  #    nat_gateway_id = aws_nat_gateway.main.id
+  # }
 
   tags = merge (
         var.common_tags,
@@ -134,6 +134,29 @@ resource "aws_route_table" "database" {
         var.database_route_table_tags
     )
 }
+
+#always add route seperately.
+resource "aws_route" "public" {
+  route_table_id            = aws_route_table.public.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.main.id
+}   
+
+
+#always add route seperately.
+resource "aws_route" "private" {
+  route_table_id            = aws_route_table.private.id
+  destination_cidr_block    = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.main.id
+}   
+
+#always add route seperately.
+resource "aws_route" "database" {
+  route_table_id            = aws_route_table.database.id
+  destination_cidr_block    = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.main.id
+} 
+
 # associate public route table with public subnets
 resource "aws_route_table_association" "public" {
   count = length(var.public_subnet_cidr)
@@ -165,3 +188,4 @@ resource "aws_db_subnet_group" "roboshop" {
   var.db_subnet_group_tags
   )
 }
+
